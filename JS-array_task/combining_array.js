77794b21dc8bf39daaa2f016f1e9f9f696fba11f
riya1689 <riyaratri24@gmail.com>
2025-07-01 10:57:53 +0600

    JS-array-task

---
+++ JS-array_task/combining_array.js
@@ -0,0 +1,11 @@
+// Create two arrays of your choice.
+const arr1 =['add', 'remove', 'subtract','multiply', 'division'];
+
+const arr2 = ['sajek', 'coxs bazar', 'sundarban'];
+// Using the concat method to combine the two arrays into a new array.
+const arr3 = arr1.concat(arr2);
+// Printing both the original arrays and the combined array using console.log().
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
---
+++ JS-array_task/declare_an_array.js
@@ -0,0 +1,4 @@
+const fruits =['apple', 'lemon', 'banana', 'blackberry', 'orange'];
+console.log(fruits[3]);//showing 3rd index element
+fruits[2] = 'watermelon';
+console.log(fruits);
