d76259ac150724be6bcc5b0329bbc299c52a0fd0
riya1689 <riyaratri24@gmail.com>
2025-08-08 14:28:19 +0600

    chrome devtool features

---
+++ chrome-devtool-features.js/console-table.js
@@ -0,0 +1,5 @@
+const friends =[{a:2,b:3}, {a:2,b:3},{a:2,b:3,d:5},{a:2,b:3},{a:2,b:3},{a:2,b:3,d:5},]
+console.log(friends);
+
+console.table(friends);
+//console devtool API
---
+++ chrome-devtool-features.js/time.js
@@ -0,0 +1,6 @@
+console.time('loop')
+for(let i=0;i<1009;i++){
+    5+58*2;
+
+}
+console.timeEnd('loop');
