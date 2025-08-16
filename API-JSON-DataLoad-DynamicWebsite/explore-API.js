deed8af8708a7e55ea2a878e6c87aed606de2f21
riya1689 <riyaratri24@gmail.com>
2025-08-16 18:21:50 +0600

    basic API

---
+++ API-JSON-DataLoad-DynamicWebsite/explore-API.js
@@ -0,0 +1,5 @@
+/**
+ * Application Programming Interface (API) 
+ * communicate with server and client side using a set of defination and protocols.
+ * Client side -> API -> Server ->Database ->server ->API ->Client side
+ */
