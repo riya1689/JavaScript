255c6ad23ff3bdcf56b2db98777e35fae37dd964
riya1689 <riyaratri24@gmail.com>
2025-09-10 03:46:30 +0600

    added important eight topic

---
+++ more-advance-JS/important-eight.js
@@ -0,0 +1,17 @@
+/**
+ * 1.How to fetch Data from API using Fetch or Async Await.Data inside array or object?
+ * 
+ * 2.How to use arrow function
+ * 
+ * 3.How to data dynamically in template string
+ * 
+ * 4.map, forEach, Filter, Find when to use?what is the relation among them?
+ * 
+ * 5.Let, Const, Var difference?when to use?
+ * 
+ * 6.loop inside array , loop inside object
+ * 
+ * 7.How to use spread, copy array using spread
+ * 
+ * 8.Destructure of object or array and declare variable in ES6
+ */
